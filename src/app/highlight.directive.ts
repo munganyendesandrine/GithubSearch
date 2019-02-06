e60816f8df0b6dd931be5a70d5fcebe249f0af0f
munganyendesandrine <mungasandwingsd@gmail.com>
2019-02-06 11:25:22 +0200

    Custom directive working on Highlight clickOnce=aqua, Doubleclick=green

--- src/app/highlight.directive.ts
+++ src/app/highlight.directive.ts
@@ -1,10 +1,24 @@
-import { Directive } from '@angular/core';
+
+import { Directive,ElementRef,HostListener } from '@angular/core';
 
 @Directive({
   selector: '[appHighlight]'
 })
 export class HighlightDirective {
 
-  constructor() { }
+  constructor(private elem:ElementRef){}
+  @HostListener("click") onClicks(){
+    this.background("aqua")
+  }
 
+  @HostListener("dblclick") onDoubleClicks(){
+    this.background("green")
+  }
+
+  private background(action:string){
+    this.elem.nativeElement.style.background=action;
+
+  }
+
+  
 }
